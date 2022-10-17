a05375575e5bab5da935bc29170ea98ae023b030
al71838@alunos.utad.pt <al71838@alunos.utad.pt>
2022-10-17 19:41:41 +0100

    add login/signup pages not finished

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,16 +1,50 @@
-<template>
-    <main>
-        <h1>Login Page</h1>
+<template >
+    <main style="background-color: var(--color-background-nav); height: 100%;">
+        <div class="padding-10 border-radius-20 " style="background-color: white;">
+            <div>
+                <a  href="">
+                    <img class="border-radius-20" style="background-color: var(--color-background-nav);" height="100" src="@/assets/RasBet_logo.png">
+                </a>
+                <h1 style="color: var(--color-background-nav);">BEM VINDO</h1>
+                <br>
+                <input placeholder= "Email" class="border-radius-20">
+                <br>
+                <input placeholder= "Password" class="border-radius-20">
+                <br>
+                <a style="color: var(--color-background-nav);" href="">Esqueci-me da palavra-passe</a>
+                <br>
+                <button class="border-radius-20 login-button padding-10"><H4 style="color: var(--color-background-nav);">Aceder</H4></button>
+                <br>
+                <p>Não tem conta?</p>
+                <a style="color: var(--color-background-nav);" href="@/pages/SignUpPage.vue">Registe-se já</a>
+            </div>
+            <div><img class="border-radius-20 padding-10" src="@/assets/Secundary_Image1.jpg"></div>
+
+
+        </div>
+
     </main>
 </template>
 
 <script>
 
-export default{
+export default {
     name: "LoginPage",
 }
 
 </script>
 
 <style scoped>
+.login-button {
+    background-color: var(--color-odd-selected);
+    width: 130px;
+    justify-content: center;
+    display: flex;
+    justify-content: center;
+    cursor: pointer;
+}
+
+input.placeholder {
+   text-align: center; 
+}
 </style>
